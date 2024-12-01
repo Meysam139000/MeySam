4d599a37493cee5e168451bdb31252d362050722
Meysam139000 <meysam.mousavi029@gmail.com>
2024-12-01 19:54:07 +0330

    Update Yadegari.py

--- Yadegari.py
+++ Yadegari.py
@@ -3,4 +3,21 @@ print("I'M ' MeySam ' DaNiEL")
 print("KLR Shad , Sourosh")
 print("1403 or 2024")
 print("My friend is Hosein")
-print(f"{blue} ")
+import os
+os.system("pip install colored")
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# rang
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+
